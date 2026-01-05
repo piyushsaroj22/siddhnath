2070ab3d1f60ee7adf1195a29b488223658884a0
Piyush Saroj <piyushsaroj222006@gmail.com>
2026-01-05 20:29:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,60 @@
+const form = document.getElementById("contactForm");
+const statusText = document.getElementById("formStatus");
+
+form.addEventListener("submit", function(e) {
+    e.preventDefault(); // ⛔ default redirect stop
+
+    statusText.textContent = "Sending message...";
+    statusText.style.color = "#c7c7c7";
+
+    const formData = new FormData(form);
+
+    fetch(form.action, {
+            method: "POST",
+            body: formData,
+            headers: {
+                "Accept": "application/json" // ⭐ VERY IMPORTANT
+            }
+        })
+        .then(response => response.json())
+        .then(result => {
+            if (result.success) {
+                statusText.textContent = "✅ Message sent successfully! We will contact you soon.";
+                statusText.style.color = "#4f80ff";
+                form.reset();
+            } else {
+                statusText.textContent = "❌ Something went wrong. Please try again.";
+                statusText.style.color = "#ff6b6b";
+            }
+        })
+        .catch(() => {
+            statusText.textContent = "❌ Network error. Please try later.";
+            statusText.style.color = "#ff6b6b";
+        });
+});
+
+
+
+
+
+
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll("nav ul li a");
+
+window.addEventListener("scroll", () => {
+    let current = "";
+
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop - 120;
+        if (scrollY >= sectionTop) {
+            current = section.getAttribute("id");
+        }
+    });
+
+    navLinks.forEach(link => {
+        link.classList.remove("active");
+        if (link.getAttribute("href") === "#" + current) {
+            link.classList.add("active");
+        }
+    });
+});
